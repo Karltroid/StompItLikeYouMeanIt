6b9ea64fc9f1db7a858a525e690426da5d385e5e
Karl <KarltroidContact@Gmail.com>
2024-01-28 16:22:18 -0500

    Initial commit

---
+++ src/main/java/me/karltroid/stompitlikeyoumeanit/StompItLikeYouMeanIt.java
@@ -0,0 +1,30 @@
+package me.karltroid.stompitlikeyoumeanit;
+
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class StompItLikeYouMeanIt extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    private void onCropTrample(PlayerInteractEvent event)
+    {
+        if (event.getAction() != Action.PHYSICAL) return;
+        if (event.getPlayer().isSneaking()) return;
+
+        Block block = event.getClickedBlock();
+        if (block == null || block.getType() != Material.FARMLAND) return;
+
+        event.setCancelled(true);
+    }
+}
